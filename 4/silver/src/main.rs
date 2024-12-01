d18964d403472fc563864b721fa618970919dc5c
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2024-12-01 20:09:19 +0100

    day 4 silver

---
+++ 4/silver/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let input = "ckczppom";
+
+    for i in 0..std::u32::MAX {
+        let input = input.to_owned() + &i.to_string();
+        let digest = md5::compute(&input);
+
+        if digest.0[0] == 0 && digest.0[1] == 0 && digest.0[2] <= 0x0F {
+            println!("{}", input);
+            break;
+        }
+    }
+}
